e657c501b5f20bac43f4035b1f32421732663891
Ajith D <ajithd78564@gmail.com>
2022-03-01 12:53:44 +0530

    live chat blade

---
+++ resources/views/pages/livechat/livechat.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.app')
+@section('main_section')
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="row">
+              <div class="col-md-6 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <div><h2>There is no class in 3rd hour</h2></div>
+                    <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                      <div class="text-md-center text-xl-left">
+                        <h6 class="mb-1">AGGIE VARGHESE</h6>
+                        <p class="text-muted mb-0">Date: 07 Jan 2019</p>
+                        <p class="text-muted mb-0">Time: 10:30 AM</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 grid-margin stretch-card">
+                  <div class="card">
+                    <div class="card-body">
+                      <div><h2>The 3rd Hour is SHYMA A</h2></div>
+                      <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                        <div class="text-md-center text-xl-left">
+                          <h6 class="mb-1">AGGIE VARGHESE</h6>
+                          <p class="text-muted mb-0">Date: 07 Jan 2019</p>
+                          <p class="text-muted mb-0">Time: 10:30 AM</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <!-- content-wrapper ends -->
+@endsection
