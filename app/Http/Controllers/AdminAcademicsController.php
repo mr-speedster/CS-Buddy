4fe02b33b2566633c3c88ef297b46f89bf4f99af
Ajith D <ajithditto@gmail.com>
2022-03-09 11:17:32 +0530

    updated with new icon and academics added

--- app/Http/Controllers/AdminAcademicsController.php
+++ app/Http/Controllers/AdminAcademicsController.php
@@ -13,7 +13,7 @@ class AdminAcademicsController extends Controller
      */
     public function index()
     {
-        return view('admin.pages.StudentDetails.academics');
+        return view('admin.pages.Academics.academics');
     }
 
     /**
---
+++ resources/views/admin/pages/Academics/academics.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layout.app')
+@section('main_section')
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="row">
+              <div class="col-md-6 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Attendance</h4>
+                     
+                    <form class="forms-sample" action="" method="POST">
+                      <div class="form-group">
+                        <label for="exampleInputUsername1">Subject</label>
+                        <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Subject">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Teacher</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Teacher">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">Date</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Date">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputConfirmPassword1">Time</label>
+                        <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Current Time">
+                      </div>
+                      <button type="submit" class="btn btn-primary me-2">Submit</button>
+                      <button class="btn btn-dark">Cancel</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+        </div>
+          <!-- content-wrapper ends -->
+@endsection
